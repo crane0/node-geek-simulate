5197437a6f50b333fb60497908e9239703532732
crane0 <bhad5683126@163.com>
2020-02-19 10:41:10 +0800

    refactor: 性能调优
    以download 页面为例子，调整了 readFileSync 和 byteLengthUtf8 引起的高 CPU占用

--- download/index.js
+++ download/index.js
@@ -7,14 +7,17 @@ const app = new koa()
 
 // 如果匹配到静态文件，就在指定目录中返回。
 // 如果没有这个逻辑，则所有的请求都会返回 source/index.htm
-console.log(__dirname + '/source/')
 app.use(
   static(__dirname + '/source/')
 )
 
+const buffer = fs.readFileSync(__dirname + '/source/index.htm')
 app.use(
   mount('/', async (ctx) => {
-    ctx.body = fs.readFileSync(__dirname + '/source/index.htm', 'utf-8')
+    ctx.status = 200
+    // 如果 body 是 buffer 类型，可能会直接下载，所以需要指定类型。
+    ctx.type = 'html'
+    ctx.body = buffer
   })
 )
 
